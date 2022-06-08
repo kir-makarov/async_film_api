e49201d4efc914603b1510704fac0550eb63bb05
balyakin <92801118+balyakin@users.noreply.github.com>
2022-06-08 09:12:41 +0300

    added authenticate_user decorator

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -1,17 +1,47 @@
+import json
+from functools import wraps
 from http import HTTPStatus
+
+import aiohttp
+from aiohttp import ClientConnectionError
 from fastapi import APIRouter, Depends, HTTPException, Request
-from services.film import FilmService, get_film_service
-from models.film import FullFilm, ShortFilm
 from fastapi_pagination import Page, paginate
 from typing import Optional, List
-from core.config import error
+
+from core import const
+from services.film import FilmService, get_film_service
+from models.film import FullFilm, ShortFilm
+from core.config import error, settings
 from utils.get_param import get_params
 
 router = APIRouter()
 
+def authenticate_user(func):
+    @wraps(func)
+    async def wrapper(*args, request: Request, **kwargs):
+        auth_header = request.headers.get("Authorization")
+        headers = {"Authorization": auth_header}
+        result_role = const.ACCESS_GUEST
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(settings.AUTH_VALIDATION_URL,
+                                        headers=headers) as resp:
+                    content = await resp.content.read()
+                    content_json = json.loads(content.decode())
+                    verified = content_json.get("verified")
+                    if verified:
+                        result_role = content_json.get("role", const.ACCESS_GUEST)
+        except ClientConnectionError: # in case of /auth service unavailable
+            result_role = const.ACCESS_GUEST
+
+        kwargs['role'] = result_role
+        return await func(*args, request, **kwargs)
+    return wrapper
 
 @router.get('/', response_model=List[ShortFilm])
 @router.get('/search', response_model=List[ShortFilm])
+@authenticate_user
 async def many_films(
         request: Request,
         film_service: FilmService = Depends(get_film_service),
